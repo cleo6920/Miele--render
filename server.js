0d74b5a15c2759da827c55de61c0dacc569fa810
cleo6920 <althea12830@gmail.com>
2026-08-13 10:51:46 +0200

    Add Render Express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const createCheckoutSession = require('./api/create-checkout-session');
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+app.disable('x-powered-by');
+app.use(express.json({ limit: '1mb' }));
+app.post('/api/create-checkout-session', createCheckoutSession);
+app.use(express.static(__dirname));
+app.get('/', (_req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`[Miele Artigianale] Server avviato sulla porta ${PORT}.`);
+});
