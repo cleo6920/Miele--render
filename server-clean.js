acd677eb8092214c83a5935e77de4e2a019bb1ab
cleo6920 <althea12830@gmail.com>
2026-08-29 12:28:34 +0200

    Align clean server routes with integrated site

--- server-clean.js
+++ server-clean.js
@@ -20,6 +20,26 @@ app.use('/images', express.static(path.join(ROOT, 'images'), {
   maxAge: '1h'
 }));
 
+const send = (file) => (_req, res) => res.sendFile(path.join(ROOT, file));
+
+// Stessa struttura di navigazione del sito integrato attuale.
+app.get('/', send('home.html'));
+app.get('/home', send('home.html'));
+app.get('/centro', send('centro.html'));
+app.get('/alveoterapia', send('alveoterapia.html'));
+app.get('/bacheca', send('bacheca.html'));
+app.get('/chi-siamo', send('chi-siamo.html'));
+app.get('/contatti', send('contatti.html'));
+
+// Lo shop resta integrato nello stesso sito e mantiene tutti gli alias attuali.
+app.get('/shop', send('index-clean.html'));
+app.get('/shop.html', send('index-clean.html'));
+app.get('/index.html', send('index-clean.html'));
+
+app.get('/success', send('success.html'));
+app.get('/cancel', send('cancel.html'));
+
+// Asset e pagine statiche serviti senza riscritture runtime.
 app.use(express.static(ROOT, {
   extensions: ['html'],
   index: false,
@@ -27,20 +47,8 @@ app.use(express.static(ROOT, {
   lastModified: true
 }));
 
-const send = (file) => (_req, res) => res.sendFile(path.join(ROOT, file));
-
-app.get('/', send('centro.html'));
-app.get('/shop', send('index-clean.html'));
-app.get('/centro', send('centro.html'));
-app.get('/alveoterapia', send('alveoterapia.html'));
-app.get('/chi-siamo', send('chi-siamo.html'));
-app.get('/contatti', send('contatti.html'));
-app.get('/bacheca', send('bacheca.html'));
-app.get('/success', send('success.html'));
-app.get('/cancel', send('cancel.html'));
-
 app.use((_req, res) => res.status(404).type('text/plain').send('Pagina non trovata'));
 
-app.listen(PORT, () => {
-  console.log(`[Miele Clean] Server avviato sulla porta ${PORT}. HTML stabilizzato, nessuna riscrittura DOM runtime continua.`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`[Miele Clean] Server integrato avviato sulla porta ${PORT}. Centro + shop nello stesso progetto, nessuna riscrittura DOM runtime continua.`);
 });
